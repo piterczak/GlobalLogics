784afba9eb76842cbb4e29ec248f8c2fdbc92994
piterczak <piterczak@gmail.com>
2022-05-19 23:46:46 +0200

    changed "sklep" to "shop" to maintain one language

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from typing import Union
 from fastapi import FastAPI
 
 
-sklep = FastAPI()
+shop = FastAPI()
 
-@sklep.get("/")
-def read_front_page():
+@shop.get("/")
+def root():
     return {"Hello": "World"}
 
-@sklep.get("/products/product/{item_id}")
+@shop.get("/products/product/{item_id}")
 def read_products(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 

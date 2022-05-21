e715d5e01ccb17fa24be8c1f8643ffb47995eeb6
piterczak <piterczak@gmail.com>
2022-05-21 15:43:52 +0200

    Created api for buying products

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 from typing import Union, List
 from fastapi import FastAPI
 from models import Product
+import click
+import json
 
 #Application named Shop created with FastAPI
 shop = FastAPI()
@@ -32,31 +34,58 @@ database: List[Product] = [
     ), 
 ]
 
-print("Please provide what you'd like to do")
-while True:
-    print("Choose 1 to show current status of warehouse items")
-    print("Choose 2 to buy products")
-    print("Choose 3 to exit shop")
-    option = input()
-    if option == "1":
-        @shop.get("/")  #Navigate to main page
-        async def root():
-            counter = 0
-            temp_dict = {"Name": [], "Warehouse_count": []}
-            while counter < len(database):
-                temp_dict["Name"].append(database[counter].name)
-                temp_dict["Warehouse_count"].append(database[counter].warehouse_count)
-                counter += 1
-            return {"List of products ":temp_dict}
-    if option == "2":
-        @shop.post("/product/{product_name}/buy_cout/{buy_count}")
-        async def buy_product():
-            product_name = "product_name"
-            buy_count = "buy_count"
-            
 
-    if option == "3":
-        break
+@shop.get("/")  #Navigate to main page
+async def root():
+    return{"Welcome to the shop": "!"}
+
+
+"""print("Please provide what you'd like to do")
+#while True:
+print("Choose 1 to show current status of warehouse items")
+print("Choose 2 to buy products")
+print("Choose 3 to exit shop")
+option = input()
+if option == "1":"""
+@shop.get("/products_list")  #Navigate to main page
+async def product_list():
+    counter = 0
+    temp_dict = {"Name": [], "Warehouse_count": []}
+    while counter < len(database):
+        temp_dict["Name"].append(database[counter].name)
+        temp_dict["Warehouse_count"].append(database[counter].warehouse_count)
+        counter += 1
+    """temp_json = json.dumps(database)
+    with open(temp_json) as json_file:
+        data = json.load(json_file)
+        print(data)"""
+    return {"List of products ":temp_dict}
+"""if option == "2":"""
+
+@shop.get("/products_list/{product_name}/buy_count/{buy_count}")
+async def buy_product(product_name: str, buy_count: int):
+    product_name = product_name
+    buy_count = buy_count
+    temp_dict = {"Name": [], "Warehouse_count": []}
+    counter = 0
+    while counter < len(database):
+        temp_dict["Name"].append(database[counter].name)
+        temp_dict["Warehouse_count"].append(database[counter].warehouse_count)
+        counter += 1
+    counter = 0
+    while counter < len(database):
+        if temp_dict["Name"][counter] == product_name:
+            if temp_dict["Warehouse_count"][counter] >= buy_count:
+                temp_dict["Warehouse_count"][counter] = temp_dict["Warehouse_count"][counter] - buy_count
+            else:
+                return {"Can't buy following item because there's not enough in stock": temp_dict["Name"][counter] + " In stock: "+  str(temp_dict["Warehouse_count"][counter])}
+
+        counter += 1
+    return {"List of products ": temp_dict}
+
+
+"""   if option == "3":
+    break"""
 
 #@shop.post("/buy/{product_name}/")
 

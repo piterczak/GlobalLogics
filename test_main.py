93a1ca14bdf0c11cf4b212219e97d4002ed53eab
piterczak <piterczak@gmail.com>
2022-05-22 17:19:45 +0200

    Pep-08 changes to test_main.py (blank lines)

--- test_main.py
+++ test_main.py
@@ -3,29 +3,35 @@ from main import shop
 
 client = TestClient(shop)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Welcome to the shop": "!"}
+
 
 def test_product_list():
     response = client.get("/products_list")
     assert response.status_code == 200
     assert type(response.json()['List of products ']) == list
 
+
 def test_wrong_url():
     response = client.get("/mocked_url")
     assert response.status_code == 404
+
 
 def test_buying_wrong_product():
     response = client.get("/products_list/Juice/buy_count/5")
     assert response.status_code == 200
     assert response.json() == ["There's no item like that in warehouse!"]
 
+
 def test_buying_good_product():
     response = client.get("/products_list/Apples/buy_count/5")
     assert response.status_code == 200
     assert type(response.json()['List of products ']) == list
+
 
 def test_buying_wrong_count():
     response = client.get("/products_list/Apples/buy_count/0")
@@ -34,4 +40,3 @@ def test_buying_wrong_count():
     assert response.json() == ["Can't buy 0 items!"]
     assert response_2.status_code == 200
     assert type(response_2.json()["Not enough items in warehouse! "])
-
